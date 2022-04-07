79fb168831d445d5e1060b0e7f0923d3c2677f06
Agustinpereira07 <101513567+Agustinpereira07@users.noreply.github.com>
2022-04-07 15:27:19 -0300

    Add files via upload

---
+++ tarea.py
@@ -0,0 +1,18 @@
+numero1=int(input("ingrese primer numero:"))
+numero2=int(input("ingrese segundo numero:"))
+simbolo=input("que operacion desea hacer:")
+
+if simbolo=="+":
+    resultado=numero1+numero2
+    print(resultado)
+
+
+if simbolo=="-":
+    resultado=numero1-numero2
+    print(resultado)
+if simbolo=="/":
+    resultado=numero1/numero2
+    print(resultado)
+if simbolo=="*":
+    r=numero1*numero2
+    print(r)
